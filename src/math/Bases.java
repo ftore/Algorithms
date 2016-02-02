1457451b907da5cb2436a1bbb2557a51cfe1ac58
Rakhimov Akmal <ra07@kinx.net>
2016-02-02 11:08:31 +0900

    coded fromdecimal

--- src/math/Bases.java
+++ src/math/Bases.java
@@ -13,4 +13,17 @@ public class Bases {
 		
 		return result;
 	}
+	
+	public int fromDecimal(int n, int b) {
+		int result = 0;
+		int multiplier = 1;
+		
+		while(n > 0) {
+			result += n % b * multiplier;
+			multiplier *= 10;
+			n /= b;
+		}
+		
+		return result;
+	}
 }
