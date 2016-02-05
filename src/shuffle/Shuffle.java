dfebdcf4010321b6402ee8ec76b92d86119eb225
Rakhimov Akmal <ra07@kinx.net>
2016-02-05 11:52:25 +0900

    Coded shuffle algorithm from Sedgewick

---
+++ src/shuffle/Shuffle.java
@@ -0,0 +1,20 @@
+package shuffle;
+
+/**
+ * Knuth shuffle algorithm
+ * 
+ * @author Akmal
+ *
+ */
+public class Shuffle {
+	public static void shuffle(int[] A) {
+		int N = A.length;
+		
+		for(int i = 0; i < N; i++) {
+			int r = i + (int) (Math.random() * (N - i));
+			int tmp = A[r];
+			A[r] = A[i];
+			A[i] = tmp;
+		}
+	}
+}
